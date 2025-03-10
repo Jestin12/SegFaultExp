0f599dd38ba45f881584519440c48e1d30f85409
alfieeagle <alfieeagle@gmail.com>
2025-03-10 19:07:21 +1100

    Jacobian computed and singularites detected

--- ArmKinematics.py
+++ ArmKinematics.py
@@ -1,22 +1,52 @@
 import numpy as np
-from sympy import Matrix
 
 class ArmKinematics:
+
+    JOINTS = 6
+    TRANSFORM_DIM = 4
+    WORKSPACE_DIM = 3
 
     # Class constructor 
     def __init__(self, DHTable):
         self.DHTable = DHTable
 
 
-    # def checkCorrectness(self, ):
+    #Check for singularites using Jacobian matrices
+    def checkCorrectness(self):
+        #Initialise the position and rotation vectors
+        positionVector = np.array([np.zeros(self.WORKSPACE_DIM) for _ in range(self.JOINTS)])
+        rotationVector = np.array([np.eye(self.WORKSPACE_DIM) for _ in range(self.JOINTS)])
+        linearVelocity = np.array([np.zeros(self.WORKSPACE_DIM) for _ in range(self.JOINTS)])
+        angularVelocity = np.array([np.zeros(self.WORKSPACE_DIM) for _ in range(self.JOINTS)])
+
+        #Obtain the position, rotation, linear velocity and angular velocity vectors from the joint pose
+        for frame_num in range(self.JOINTS):
+            positionVector[frame_num] = self.jointPoseGlob[frame_num][:self.WORKSPACE_DIM, -1]
+            rotationVector[frame_num] = self.jointPoseGlob[frame_num][:self.WORKSPACE_DIM, :self.WORKSPACE_DIM]
+            angularVelocity[frame_num] = rotationVector[frame_num][:, -1]
+            linearVelocity[frame_num] = np.cross(angularVelocity[frame_num], positionVector[-1] - positionVector[frame_num])
+
+        #Compute the Jacobian matrix
+        J = np.zeros((6,6))
+        J = np.vstack((np.transpose(linearVelocity), np.transpose(angularVelocity)))
+
+        #Check for singularites
+        if np.linalg.det(J) == 0:
+            print("Singularities detected")
+        else:
+            print("No singularities detected")
+
+    def endeffectorPosition(self):
+        return self.jointPoseGlob[-1][:self.WORKSPACE_DIM, -1]
 
 
     # Determine the final end effector position 
-    def endEffectorPose(self):
-        finalTransform = np.eye(4)
+    def getAllJointPose(self):
+        self.jointPoseGlob = np.array([np.eye(self.TRANSFORM_DIM) for _ in range(self.JOINTS)])
 
-        for frame_num in range(6):
-            finalTransform = finalTransform@self.DHTable.constructHT(self.DHTable.DH_Table, frame_num)
+        for frame_num in range(self.JOINTS):
+            self.jointPoseGlob[frame_num] = self.jointPoseGlob[frame_num-1]@self.DHTable.constructHT(self.DHTable.DH_Table, frame_num)
 
-        return finalTransform, finalTransform[0,3], finalTransform[1,3], finalTransform[2,3]
+        return self.jointPoseGlob
+    
     
--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from DHTable import DHTable
 from ArmKinematics import ArmKinematics
 import math
 
-joint_angles = [math.pi, math.pi, 2*(math.pi), -(math.pi), 0, 0]
+joint_angles = [0,0,0,0,0,0]
 
 table1 = DHTable(joint_angles)
 
 kinematics = ArmKinematics(table1)
 
-mat, x,y,z = kinematics.endEffectorPose()
+print("Transform Matrices: \n", kinematics.getAllJointPose().round(2), "\n")
+
+print("End Effector Position: \n",kinematics.endeffectorPosition().round(2), "\n")
+
+kinematics.checkCorrectness()
