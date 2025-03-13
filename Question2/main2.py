5f5531606e110d5b59e9e0fdfa90cc342622f47f
Jestin <jestinji@outlook.com>
2025-03-13 16:54:25 +1100

    changed the q2 dh table

---
+++ Question2/main2.py
@@ -0,0 +1,30 @@
+from DHTable2 import DHTable
+from ArmKinematics2 import ArmKinematics
+from ArmVisualiser2 import ArmVisualiser
+import math
+
+joint_angles = [0,0,0,0]
+
+table1 = DHTable(joint_angles, 100, 100)
+
+
+print(table1.constructHT(0))
+print(table1.constructHT(1))
+print(table1.constructHT(2))
+print(table1.constructHT(3))
+
+
+kinematics = ArmKinematics(table1)
+
+transforms =  kinematics.getAllJointGlobPose()
+
+#print("Joint Positions: \n", transforms.round(2), "\n")
+
+print("End Effector Position: \n",kinematics.endeffectorPosition().round(2), "\n")
+
+kinematics.checkCorrectness()
+
+visualiser = ArmVisualiser()
+visualiser.PlotUR5e(transforms)
+
+
