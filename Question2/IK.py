df56dbcbc17595fbff7ed98c78eeb3537ad03de2
alfieeagle <alfieeagle@gmail.com>
2025-03-14 15:51:05 +1100

    Got da mudda fucka workin

--- Question2/IK.py
+++ Question2/IK.py
@@ -1,32 +1,32 @@
-from sympy import symbols, Eq, solve, sqrt, atan2, acos
+from sympy import symbols, Eq, solve, sqrt, atan2, acos, cos, sin, pprint
 from ArmVisualiser2 import ArmVisualiser
 from ArmKinematics2 import ArmKinematics
+import numpy as np
 
 # Define symbolic variables
 theta_1, theta_2 = symbols('theta_1 theta_2')
 
 # Given values
-x, y = 750, -50  # Target position of end effector
-s1, s4 = 500, 50  # Additional link parameters
-L1, L2 = 600, 450 + s4  # Link lengths
-
-# Compute r using SymPy
-r = sqrt(x**2 + y**2)
+x, y = 750, 0  # Target position of end effector
+s1, s4 = 500, 50  # Offset values
+L2, L3 = 600, 450 + s4  # Link lengths
 
 # Define inverse kinematics equations
-eq1 = Eq(theta_2, acos((x**2 + y**2 - L1**2 - L2**2) / (2 * L1 * L2)))
-eq2 = Eq(theta_1, atan2(y, x) - acos((r**2 + L1**2 - L2**2) / (2 * L1 * r)))
+eq1 = Eq(x, L2*cos(theta_1) + L3*cos(theta_1 + theta_2))
+eq2 = Eq(y, s1 + L2*sin(theta_1) + L3*sin(theta_1 + theta_2))
 
 # Solve the system
 solution = solve((eq1, eq2), (theta_1, theta_2))
+pprint(solution)
 
-kinematics = ArmKinematics(table1)
+theta_value_1 = solution[0][0].evalf()
+theta_value_2 = solution[0][1].evalf()
 
-transforms =  kinematics.getAllJointGlobPose()
+theta_value_3 = solution[1][0].evalf()
+theta_value_4 = solution[1][1].evalf()
 
-kinematics.checkCorrectness()
+print("Theta 1: ", theta_value_1)
+print("Theta 2: ", theta_value_2)
+print("Theta 3: ", theta_value_3)
+print("Theta 4: ", theta_value_4)
 
-visualiser = ArmVisualiser()
-visualiser.PlotUR5e(transforms)
-
-visualiser.Show()
--- Question2/main2.py
+++ Question2/main2.py
@@ -8,16 +8,9 @@ import numpy as np
 THETA_1 = math.radians(89.18)
 THETA_2 = math.radians(-94.53) - math.radians(90)
 
-joint_angles = [0,-0.74,1.65,0]
+joint_angles = [0,-1.137,1.227 - np.pi/2,0]
 
 table1 = DHTable(joint_angles, 500, 50)
-
-
-print(table1.constructHT(0))
-print(table1.constructHT(1))
-print(table1.constructHT(2))
-print(table1.constructHT(3))
-
 
 kinematics = ArmKinematics(table1)
 
@@ -25,17 +18,17 @@ transforms =  kinematics.getAllJointGlobPose()
 
 #print("Joint Positions: \n", transforms.round(2), "\n")
 
-# print("End Effector Position: \n",kinematics.endeffectorPosition().round(2), "\n")
+print("End Effector Position: \n",kinematics.endeffectorPosition().round(2), "\n")
 
 kinematics.checkCorrectness()
 
 # plotter = WorkspacePlotter(50, 100, 10, 30, -4*np.pi/5, np.pi/2, -4*np.pi/3, 4*np.pi/3)
-plotter = WorkspacePlotter(50, 100, 10, 30, 0, 2*np.pi, 0, 2*np.pi)
-plotter.plotWorkspace()
+# plotter = WorkspacePlotter(50, 100, 10, 30, 0, 2*np.pi, 0, 2*np.pi)
+# plotter.plotWorkspace()
 
-# visualiser = ArmVisualiser()
-# visualiser.PlotUR5e(transforms)
+visualiser = ArmVisualiser()
+visualiser.PlotUR5e(transforms)
 
-# visualiser.Show()
+visualiser.Show()
 
 
