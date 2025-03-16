563698000174cdd79737541522d7fda95f6d36d8
alfieeagle <126121089+alfieeagle@users.noreply.github.com>
2025-03-16 17:05:23 +1100

    Fixed a few things

--- Question2/main2.py
+++ Question2/main2.py
@@ -5,12 +5,15 @@ import math
 from workspacePlotter import WorkspacePlotter
 import numpy as np
 
-THETA_1 = math.radians(89.18)
-THETA_2 = math.radians(-94.53) - math.radians(90)
+THETA_2 = math.radians(30)
+THETA_3 = math.radians(60) - math.radians(90)
 
-joint_angles = [0,-0.0386,-1.227 - np.pi/2,0]
+S1 = 500
+S4 = 50
 
-table1 = DHTable(joint_angles, 500, 50)
+joint_angles = [0,THETA_2,THETA_3,0]
+
+table1 = DHTable(joint_angles, S1, S4)
 
 kinematics = ArmKinematics(table1)
 
@@ -18,18 +21,20 @@ transforms =  kinematics.getAllJointGlobPose()
 
 #print("Joint Positions: \n", transforms.round(2), "\n")
 
+np.set_printoptions(suppress=True)
+
 print("End Effector Position: \n",kinematics.endeffectorPosition().round(2), "\n")
 
 kinematics.checkCorrectness()
 
 # plotter = WorkspacePlotter(50, 100, 10, 30, -4*np.pi/5, np.pi/2, -4*np.pi/3, 4*np.pi/3)
-# plotter = WorkspacePlotter(50, 100, 10, 30, 0, 2*np.pi, 0, 2*np.pi)
+# # plotter = WorkspacePlotter(50, 100, 10, 30, 0, 2*np.pi, 0, 2*np.pi)
 # plotter.plotWorkspace()
 
-visualiser = ArmVisualiser()
-visualiser.PlotUR5e(transforms)
-visualiser.PlotObstacle([400,0], 300)
+# visualiser = ArmVisualiser()
+# visualiser.PlotUR5e(transforms)
+# visualiser.PlotObstacle([400,0], 300)
 
-visualiser.Show()
+# visualiser.Show()
 
 
