5bbb2d12ad07f7f2bed7007392e0e2f3cdb1ca34
Jestin <jestinji@outlook.com>
2025-04-24 17:48:25 +1000

    yeh yeh yeh wiki fresh

--- A3/pathfinder/src/drive/drive/driver.py
+++ A3/pathfinder/src/drive/drive/driver.py
@@ -4,6 +4,9 @@ from rclpy.node import Node
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
+TURN_RIGHT = 0.5 
+TURN_LEFT = 1.5
+
 
 class Driver(Node):
     def __init__(self):
@@ -11,10 +14,75 @@ class Driver(Node):
         
         self.MovePub = self.create_publisher(Twist, '/cmd_vel', 10)
 
-        self.SignSub = self.create_subscription(String, 'pedestrian/Signs', self.SignSub_Callback, 10)
+        self.SignSub = self.create_subscription(String, 'ModeSign', self.SignSub_Callback, 10)
+
+        self.MoveForward()
+
+    
+    def MoveForward(self): 
+        self.update_cmd_vel(2.0, 0)
 
 
-    # def SignSub_Callback(self, msg):
+    def SignSub_Callback(self, msg):
+        command = msg.data
+
+        if command == "Ahead only": 
+            pass
+
+        elif command = "Turn left": 
+            self.update_angular_vel(TURN_LEFT)
+
+        elif command == "Turn right": 
+            self.update_angular_vel(-1*TURN_RIGHT)
+
+        elif command == "Roundabout mandatory":
+            pass 
+
+        elif command == "Stop": 
+            self.Stop()
+
+
+    def update_angular_vel(self, double angular):
+
+        cmd_vel = Twist()
+
+        cmd_vel.angular.z = angular
+
+        self.MovePub.publish(cmd_vel) 
+
+
+    def update_cmd_vel(self, double linear, double angular):
+
+        cmd_vel = Twist()
+
+        cmd_vel.linear.x = linear
+        cmd_vel.angular.z = angular
+
+        self.MovePub.publish(cmd_vel) 
+
+
+    
+    def Stop(self):
+
+        cmd_vel = Twist()
+
+        cmd_vel.linear.x = 0.0
+        cmd_vel.angular.z = 0.0
+
+        self.MovePub.publish(cmd_vel) 
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Driver()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
+
 
 
 
