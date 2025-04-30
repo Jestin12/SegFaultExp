8a426b598eefba88fb791ba7d8d3193f3300cb5c
AmelineA2003 <aarc0926@uni.sydney.edu.au>
2025-04-30 14:41:04 +1000

    4 alfs

--- A3/pathfinder/src/drive/drive/coordinates.py
+++ A3/pathfinder/src/drive/drive/coordinates.py
@@ -42,75 +42,95 @@ class CoordinateFinder(Node):
 	def coordinate_callback(self, msg): 
 
 		# Convert camera points to floats 
-		command = msg.data.split() 
+		command = msg.data.split()
 
-		x = float(command[-2]) 
-		y = float(command[-1]) 
+		# self.get_logger().info(command)
 
+		if len(command) >= 3: 
 
-		# Transform from camera to LiDAR frame 
-		camera_point = np.vstack((np.array([[x], [y]]), [[1]])) 
-		
-		transformed_point = np.linalg.inv(self.K) @ camera_point
+			x = float(command[-2]) 
+			y = float(command[-1]) 
 
-		homogeneous_point = np.vstack((transformed_point, [[1]]))
-		
-		lidar_point = self.CTL @ homogeneous_point
+			# Tranform camera x,y into lidar using two rotations 
 
 
-		# Convert lidar_point into PoseStamped format 
-		lidar_point_msg = PointStamped()
-		lidar_point_msg.header.frame_id = 'lidar_link'
-		lidar_point_msg.header.stamp = self.get_clock().now().to_msg()
-		lidar_point_msg.point.x = lidar_point[0, 0]
-		lidar_point_msg.point.y = lidar_point[1, 0]
-		lidar_point_msg.point.z = lidar_point[2, 0]
+			# Transform from camera to LiDAR frame 
+			camera_point = np.vstack((np.array([[x], [y]]), [[1]])) 
+			
+			transformed_point = np.linalg.inv(self.K) @ camera_point
+
+			homogeneous_point = np.vstack((transformed_point, [[1]]))
+			
+			lidar_point = self.CTL @ homogeneous_point
+
+			# Create bound around y value to constrait LiDAR values 
 
 
-		# Find vector for movement direction 
-		direction_vector = lidar_point[:3].flatten()
-		direction_vector /= np.linalg.norm(direction_vector)
-
-		movement_vector = Vector3Stamped()
-		movement_vector.header.frame_id = 'lidar_link'  
-		movement_vector.header.stamp = self.get_clock().now().to_msg()
-		movement_vector.vector.x = direction_vector[0]
-		movement_vector.vector.y = direction_vector[1]
-		movement_vector.vector.z = direction_vector[2]
+			# Determine x value using average LiDAR depth within bounds 
 
 
+			# Change the x value 
 
 
-		# Transform from robot to Nav2 frame 
-		map_frame = 'map'
-		lidar_frame = 'base_link'
-
-		robot_transformation = self.tf_buffer.lookup_transform(map_frame, lidar_frame, rclpy.time.Time())
-
-		nav_point = tf2_geometry_msgs.do_transform_point(lidar_point_msg, robot_transformation)
-		transformed_vector = tf2_geometry_msgs.do_transform_vector3(movement_vector, robot_transformation)
-
-
-		# Add to current position of robot ?????????????????
-		# final_point = self.pose + nav_point
-
-		# new_pose_x = final_point[0]
-		# new_pose_y = final_point[1]
-		# new_pose_z = final_point[2]
-		
-
-		# Publish updated waypoint 
-		goal_pose = PoseStamped()
-
-		goal_pose.pose.position.x = nav_point.point.x
-		goal_pose.pose.position.y = nav_point.point.y
-		goal_pose.pose.position.z = nav_point.point.z
+			# Convert lidar_point into PoseStamped format 
+			lidar_point_msg = PointStamped()
+			lidar_point_msg.header.frame_id = 'base_scan'
+			lidar_point_msg.header.stamp = self.get_clock().now().to_msg()
+			lidar_point_msg.point.x = lidar_point[0, 0]
+			lidar_point_msg.point.y = lidar_point[1, 0]
+			lidar_point_msg.point.z = lidar_point[2, 0]
 
 
-		yaw = math.atan2(transformed_vector.vector.y, transformed_vector.vector.x)
-		goal_pose.pose.orientation = self.findQuarternion(yaw) 
-		
-		self.CoordinatePub.publish(goal_pose)
+			# Find vector for movement direction 
+			direction_vector = lidar_point[:3].flatten()
+			direction_vector /= np.linalg.norm(direction_vector)
+
+			movement_vector = Vector3Stamped()
+			movement_vector.header.frame_id = 'base_scan'
+			movement_vector.header.stamp = self.get_clock().now().to_msg()
+			movement_vector.vector.x = direction_vector[0]
+			movement_vector.vector.y = direction_vector[1]
+			movement_vector.vector.z = direction_vector[2]
+
+
+
+
+			# Transform from robot to Nav2 frame 
+			map_frame = 'map'
+			lidar_frame = 'base_scan'
+
+			robot_transformation = self.tf_buffer.lookup_transform(map_frame, lidar_frame, self.get_clock().now())
+
+			nav_point = tf2_geometry_msgs.do_transform_point(lidar_point_msg, robot_transformation)
+			transformed_vector = tf2_geometry_msgs.do_transform_vector3(movement_vector, robot_transformation)
+
+			robot_transformation = self.tf_buffer.lookup_transform(map_frame, lidar_frame, self.get_clock().now())
+
+			nav_point = tf2_geometry_msgs.do_transform_point(lidar_point_msg, robot_transformation)
+			# Add to current position of robot ?????????????????
+			# final_point = self.pose + nav_point
+
+			# new_pose_x = final_point[0]
+			# new_pose_y = final_point[1]
+			# new_pose_z = final_point[2]
+			
+
+			# Publish updated waypoint 
+			goal_pose = PoseStamped()
+
+			# goal_pose.header.stamp = self.get_clock().now()
+			# goal_pose.header.frame_id = "map"  
+
+
+			goal_pose.pose.position.x = nav_point.point.x
+			goal_pose.pose.position.y = nav_point.point.y
+			goal_pose.pose.position.z = nav_point.point.z
+
+
+			yaw = math.atan2(transformed_vector.vector.y, transformed_vector.vector.x)
+			goal_pose.pose.orientation = self.findQuarternion(yaw) 
+			
+			self.CoordinatePub.publish(goal_pose)
 
 	
 	def findQuarternion(self, yaw):
