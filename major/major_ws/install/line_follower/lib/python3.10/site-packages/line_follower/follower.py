2ac8d045cb9ed0dddd6a12041f05e5d88c3bf7e7
AmelineA2003 <aarc0926@uni.sydney.edu.au>
2025-05-15 15:09:35 +1000

    added major WS

---
+++ major/major_ws/install/line_follower/lib/python3.10/site-packages/line_follower/follower.py
@@ -0,0 +1,56 @@
+import RPI.GPIO as GPIO 
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist 
+from std_msgs.msg import String
+
+import os
+
+class Follower(Node): 
+	def __init__(self): 
+		super().__init__("Follower")
+
+		self.CENTRE = 25
+		
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(self.CENTRE, GPIO.IN)
+
+		self.vel_publisher = self.create_publisher(Twist, "/cmd_vel", 10)
+		self.IR_publisher = self.create_publisher(String, "/IR_value", 10)
+
+		self.timer = self.create_timer(0.05, self.check_sensors)
+
+
+	def check_sensors(self): 
+
+		value = GPIO.input(self.CENTRE)
+
+		vel_msg = Twist()
+		ir_msg = String()
+
+		if value == 0: 
+
+			vel_msg.linear.x = 0.5
+			vel_msg.angular.z = 0.0 
+
+		else: 
+			vel_msg.linear.x = 0.0
+			vel_msg.angular.z = 0.0 
+
+
+		ir_msg.data = str(value)
+
+		self.vel_publisher.publish(vel_msg)
+		self.IR_publisher.publish(ir_msg)
+
+
+
+def main(args=None): 
+	rclpy.init(args=args)
+	node = Follower()
+	rclpy.spin(node)
+	rclpy.shutdown()
+
+
+if __name__ == "__main__": 
+	main()
